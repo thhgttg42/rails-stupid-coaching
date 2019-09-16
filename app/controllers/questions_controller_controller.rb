eb30d5dd1dfc8168e493617c1aff7770b48bc37f
Andre Lauritzen <lauritzen.42@gmail.com>
2019-09-16 12:16:13 +0200

    Think the controller is created, or something like that.

---
+++ app/controllers/questions_controller_controller.rb
@@ -0,0 +1,2 @@
+class QuestionsControllerController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  #Prefix Verb URI Pattern       Controller#Action
+     # ask GET /ask(.:format)    questions#ask
+     #  answer GET /answer(.:format) questions#ans
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/ask', to: 'questions#ask'
+  get '/answer', to: 'question#answer'
 end
